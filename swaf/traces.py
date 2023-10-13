2a2c2e46839926c3feec20e93532bb44f7021e88
JeandeMontigny <31214891+JeandeMontigny@users.noreply.github.com>
2023-10-13 14:33:50 +0200

    clean code, create check_t function, improve plot_analogsig and plot_ave_waveform

--- swaf/traces.py
+++ swaf/traces.py
@@ -1,12 +1,10 @@
-import os, sys, pickle, neo
+import os, sys, inspect, neo
 import numpy as np
 import quantities as pq
 import matplotlib.pyplot as plt
 from scipy import signal
 
 from .utils import *
-
-import psutil, time
 
 # ---------------------------------------------------------------- #
 class Spike_recording:
@@ -31,25 +29,40 @@ class Spike_recording:
         # get and plot signal
 
     # ---------------- #
-    def plot_analogsig(self, plot_stim=True):
-        plt.plot(self.segment.analogsignals[2].times, self.signal, color='k')
+    def plot_analogsig(self, t_start, t_stop, plot_stim=True, show_plot=False, plot_save_path=""):
+        # catch out of recording errors
+        self.check_t(t_start, t_stop)
+
+        # get array id corresponding to t_start and t_stop
+        id_start = int((t_start-float(self.segment.t_start)) * float(self.sampling_rate))
+        id_stop = int((t_stop-float(self.segment.t_start)) * float(self.sampling_rate))
+
+        plt.plot(self.segment.analogsignals[2].times[id_start:id_stop], self.signal[id_start:id_stop], color='k')
         plt.xlabel(f"Time ({self.segment.analogsignals[2].times.units.dimensionality.string})")
         plt.ylabel(f"{self.segment.analogsignals[2].units.dimensionality.string}")
 
         if plot_stim and len(self.stim_trigger_times) > 0:
+            # get stim within t_start and t_stop
+            stim_idx = np.where((self.stim_trigger_times > t_start) & (self.stim_trigger_times < t_stop))
             # plot stimulation times
-            for trigger_time in self.stim_trigger_times:
-                plt.vlines(trigger_time, -6, -5.5, colors='r')
+            plt.vlines(self.stim_trigger_times[stim_idx], -6, -5.5, colors='r')
 
-        plt.show()
+        if len(plot_save_path) > 0:
+            if not plot_save_path[len(plot_save_path)-1] == "/":
+                plot_save_path = plot_save_path + "/"
+            # check that folder exists. if not, create it
+            if not os.path.isdir(plot_save_path):
+                os.makedirs(plot_save_path)
+            plot_save_path = plot_save_path + "analog-signal_" + str(t_start) + "-" + str(t_stop) + ".png"
+            plt.savefig(plot_save_path, dpi=720)
+
+        if show_plot:
+            plt.show()
 
     # ---------------- #
-    def get_ave_waveform(self, t_start, t_stop, plot=False):
+    def get_ave_waveform(self, t_start, t_stop, show_plot=False, plot_save_path=""):
         # catch out of recording errors
-        if t_start < self.segment.t_start:
-            raise ValueError('Specified t_start', t_start, 'is smaller than the extracted recording start time', self.segment.t_start, 'in function get_ave_waveform()')
-        if t_stop > self.segment.t_stop:
-            raise ValueError('Specified t_stop', t_stop, 'is larger than the extracted recording stop time', self.segment.t_stop, 'in function get_ave_waveform()')
+        self.check_t(t_start, t_stop)
 
         # get stims idx between t_start and t_stop
         stim_times_idx = np.where((self.stim_trigger_times > t_start) & (self.stim_trigger_times < t_stop))[0]
@@ -71,18 +84,42 @@ class Spike_recording:
         waveforms = np.transpose(waveforms)
         ave_waveform = np.average(waveforms, axis=1)
 
-        if plot:
-            plt.figure()
-            # centred arround stim time
-            plt.plot([t/self.sampling_rate for t in range(-int(np.ceil(0.1*float(self.sampling_rate))/2), int(np.ceil(0.1*float(self.sampling_rate))/2))], ave_waveform, c='k')
-            plt.xlabel(f"Time ({self.segment.analogsignals[2].times.units.dimensionality.string})")
-            plt.ylabel(f"{self.segment.analogsignals[2].units.dimensionality.string}")
-            plt.title("Waveform average: "+str(t_start)+" to "+str(t_stop)+" s")
-            plt.tight_layout()
-            plt.savefig("results/waveform-ave_" + str(t_start) + "-" + str(t_stop) + ".png", dpi=720)
-            # plt.show()
+        if show_plot or len(plot_save_path) > 0:
+            self.plot_ave_waveform(ave_waveform, t_start, t_stop, show_plot, plot_save_path)
 
         return ave_waveform
+
+    # ---------------- #
+    def plot_ave_waveform(self, ave_waveform, t_start, t_stop, show_plot, plot_save_path):
+        # catch out of recording errors
+        self.check_t(t_start, t_stop)
+
+        plt.figure()
+        # centred arround stim time
+        plt.plot([t/self.sampling_rate for t in range(-int(np.ceil(0.1*float(self.sampling_rate))/2), int(np.ceil(0.1*float(self.sampling_rate))/2))], ave_waveform, c='k')
+        plt.xlabel(f"Time ({self.segment.analogsignals[2].times.units.dimensionality.string})")
+        plt.ylabel(f"{self.segment.analogsignals[2].units.dimensionality.string}")
+        plt.title("Waveform average: "+str(t_start)+" to "+str(t_stop)+" s")
+        plt.tight_layout()
+
+        if len(plot_save_path) > 0:
+            if not plot_save_path[len(plot_save_path)-1] == "/":
+                plot_save_path = plot_save_path + "/"
+            # check that folder exists. if not, create it
+            if not os.path.isdir(plot_save_path):
+                os.makedirs(plot_save_path)
+            plot_save_path = plot_save_path + "waveform-ave_" + str(t_start) + "-" + str(t_stop) + ".png"
+            plt.savefig(plot_save_path, dpi=720)
+
+        if show_plot:
+            plt.show()
+
+    # ---------------- #
+    def check_t(self, t_start, t_stop):
+        if t_start < self.segment.t_start:
+            raise SystemExit("Error in \'" + inspect.stack()[1].function + "\' function: Specified t_start \'" + str(t_start) + "\' is smaller than the extracted recording stop time \'" + str(float(self.segment.t_start))+ "\'")
+        if t_stop > self.segment.t_stop:
+            raise SystemExit("Error in \'" + inspect.stack()[1].function + "\' function: Specified t_stop \'" + str(t_stop) + "\' is larger than the extracted recording stop time \'" + str(float(self.segment.t_stop))+ "\'")
 
 # ---------------------------------------------------------------- #
 def read_file(file_path, t_start=0, t_stop="all"):
